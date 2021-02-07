3b5b49fcbcf56e58225940a1dcc2377d7ae801bb
lspack3 <lspackman@byui.edu>
2021-02-06 21:41:35 -0700

    updates

--- lesson5/js/saturday.js
+++ lesson5/js/saturday.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
 var d = new Date ();
 
-if (d.getDay() == 6 ) {
-    document.getElementsByClassName("satBanner").style.display="block";
+if (d.getDay() == 5 ) {
+    document.getElementsByClassName("satBanner").style.display = "block";
     }
 });

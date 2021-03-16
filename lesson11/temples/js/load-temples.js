d6f4f4ca35108d1a2495696679b9cdeb544499b2
lspack3 <lspackman@byui.edu>
2021-03-16 09:07:00 -0600

    updates

--- lesson11/temples/js/load-temples.js
+++ lesson11/temples/js/load-temples.js
@@ -5,7 +5,23 @@ window.addEventListener('load', () => {
             return response.json();
         })
         .then((jsonObject) => {
-            console.log(jsonObject);
-            console.log(jsonObject[0]);
+            console.log(jsonObject); // test
+         //   console.log(jsonObject[2].presidents[0] + "is the president of the " + jsonObject[2].name);
+            Object.entries(jsonObject).forEach(([key,temple]) => {
+            //    if(temple.state == "ID"){
+                buildTempleCard(temple);
+            //    }
+            });   
         });
 });
+
+function buildTempleCard(temple) {
+    console.log(temple); // test
+    let card = document.createElement("section");
+    card.classList.add("temple")
+    card.innerHTML = `<h2>${temple.name}</h2>
+                      <img src="${temple.imageurl}" alt="${temple.name}">
+                      <p>First President: <b>${temple.presidents[0]} 1st of ${temple.presidents.length}</b></p>
+                      <p>Current President: <b>${temple.presidents[temple.presidents.length-1]}</b></p>`;
+    document.querySelector("#temples").appendChild(card);
+}
